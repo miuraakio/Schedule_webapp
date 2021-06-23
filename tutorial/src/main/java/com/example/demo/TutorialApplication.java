cc5c055a28847dc023cef99d07d7fb1c216a4705
miuraakio <pakiawsao@gmail.com>
2021-06-24 08:28:55 +0900

    不要そうな場所のコメントアウト・削除

--- tutorial/src/main/java/com/example/demo/TutorialApplication.java
+++ tutorial/src/main/java/com/example/demo/TutorialApplication.java
@@ -1,20 +1,17 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class TutorialApplication implements CommandLineRunner {
+public class TutorialApplication  {
 
 
     public static void main(String[] args) {
         SpringApplication.run(TutorialApplication.class, args);
     }
 
-    @Autowired
+    /*@Autowired
     JdbcTemplate jdbcTemplate;
 
 
@@ -25,5 +22,5 @@ public class TutorialApplication implements CommandLineRunner {
                 "id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))");
 
         jdbcTemplate.update("INSERT INTO customers(first_name, last_name) VALUES ('John','Woo')");
-    }
+    }*/
 }
