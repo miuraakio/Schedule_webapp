20f64daea9af22a4f490ef62cb05ff2151612cb2
miuraakio <pakiawsao@gmail.com>
2021-06-23 15:51:03 +0900

    sqlファイル追加、H2・jdbc追加

--- tutorial/src/main/java/com/example/demo/TutorialApplication.java
+++ tutorial/src/main/java/com/example/demo/TutorialApplication.java
@@ -1,18 +1,27 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class TutorialApplication {
+public class TutorialApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TutorialApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(TutorialApplication.class, args);
+    }
 
-		//githubテスト
-		//github反映テスト2
-		//テスト吉川
-		//test2
-	}
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
+    @Override
+    public void run(String... strings) throws Exception {
+
+        jdbcTemplate.execute("CREATE TABLE customers(" +
+                "id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))");
+
+        jdbcTemplate.update("INSERT INTO customers(first_name, last_name) VALUES ('John','Woo')");
+    }
 }
